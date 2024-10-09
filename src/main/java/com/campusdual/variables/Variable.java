5c94fad0eb459bf4841f6bb60fa10e234dc75296
EmmaIP <emmapineiro@gmail.com>
2024-10-09 13:35:49 +0200

    segundo ejercicio

---
+++ src/main/java/com/campusdual/variables/Variable.java
@@ -0,0 +1,12 @@
+package com.campusdual.variables;
+
+public class Variable {
+    public static void main(String[] args) {
+        double hours = 2.5;
+        int month = 6;
+        String name = "Leonardo";
+        name = "Emma";
+        final int DAY_HOURS = 24;
+        System.out.println(DAY_HOURS);
+    }
+}
